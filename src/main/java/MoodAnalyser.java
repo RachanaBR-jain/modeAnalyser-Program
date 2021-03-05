8e9784798623b8e3f96a037c653f600982452da0
RachanaBR-jain <br19rachanajain@gmail.com>
2021-03-05 05:58:19 +0530

    Initial commit

---
+++ src/main/java/MoodAnalyser.java
@@ -0,0 +1,15 @@
+public class MoodAnalyser {
+    public String analyseMood(String message){
+
+        if (message.contains("happy")){
+
+            return "HAPPY";
+
+        }
+        else {
+
+            return "SAD";
+
+        }
+    }
+}
---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+    public class MoodAnalyserTest {
+        MoodAnalyser mood;
+
+        @BeforeEach
+        public void objectMethod(){
+
+            mood = new MoodAnalyser();
+        }
+
+        @Test
+        public void testHappyMood(){
+
+            System.out.println(mood.analyseMood("I am in happy mood"));
+
+        }
+
+    }
+
+
